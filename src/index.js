88b8fc52912214f0510d4716087a8c443c4916ce
JFrankLL <flopez@itexico.net>
2018-04-17 15:46:41 -0500

    Configured, devdependencies and redux. Also main structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createLogger as logger } from 'redux-logger';
+import thunk from 'redux-thunk';
+import promise from 'redux-promise-middleware';
+import { Provider } from 'react-redux';
+
+const store = createStore(
+  combineReducers({}),
+  {},
+  applyMiddleware(logger(), thunk, promise()),
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
